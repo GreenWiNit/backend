3c9e95883e236a8316c55279278f896f6770aea5
jihwankim128 <jihwan1535@nate.com>
2025-05-24 20:02:44 +0900

    test: baseEntity 테스트 코드 추가 (#7)
    * isDeleted -> deleted 로 수정
    * soft delete 기능 추가
    
    getter때문에 변수명을 isDeleted로 작성하면 QEntity랑 충돌나서 수정했습니다!

--- src/main/java/com/example/green/domain/common/BaseEntity.java
+++ src/main/java/com/example/green/domain/common/BaseEntity.java
@@ -1,32 +1,41 @@
 package com.example.green.domain.common;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import lombok.Getter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
-    @CreatedDate
-    private LocalDateTime createdDate;
+	@CreatedDate
+	private LocalDateTime createdDate;
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+	@LastModifiedDate
+	private LocalDateTime modifiedDate;
 
-    // 생성자 정보 (현재는 Security 설정이 되어있지 않아 주석처리)
-    // @CreatedBy
-    private String createdBy;
+	// 생성자 정보 (현재는 Security 설정이 되어있지 않아 주석처리)
+	// @CreatedBy
+	private String createdBy;
 
-    // 수정자 정보 (현재는 Security 설정이 되어있지 않아 주석처리)
-    // @LastModifiedBy
-    private String lastModifiedBy;
+	// 수정자 정보 (현재는 Security 설정이 되어있지 않아 주석처리)
+	// @LastModifiedBy
+	private String lastModifiedBy;
 
-    private boolean isDeleted = false;
+	private boolean deleted = false;
+
+	public void markDeleted() {
+		this.deleted = true;
+	}
+
+	public void restore() {
+		this.deleted = false;
+	}
 }
