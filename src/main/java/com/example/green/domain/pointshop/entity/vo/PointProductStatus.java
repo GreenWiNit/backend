f3dedfb79acea5398e43bdc7e40cb442cc05e704
jihwankim128 <jihwan1535@nate.com>
2025-06-19 00:07:57 +0900

    test: 포인트 상품 생성 테스트 추가 (#32)

---
+++ src/main/java/com/example/green/domain/pointshop/entity/vo/PointProductStatus.java
@@ -0,0 +1,5 @@
+package com.example.green.domain.pointshop.entity.vo;
+
+public enum PointProductStatus {
+	IN_STOCK, OUT_OF_STOCK
+}
---
+++ src/main/java/com/example/green/domain/pointshop/exception/PointProductExceptionMessage.java
@@ -0,0 +1,26 @@
+package com.example.green.domain.pointshop.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.HttpStatus;
+
+import com.example.green.global.error.exception.ExceptionMessage;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum PointProductExceptionMessage implements ExceptionMessage {
+
+	INVALID_PRODUCT_CODE(BAD_REQUEST, "상품 코드는 PRD-AA-000 형식입니다."),
+	INVALID_PRODUCT_POINT(BAD_REQUEST, "상품 포인트는 0원 이상이어야 합니다."),
+	INVALID_PRODUCT_STOCK(BAD_REQUEST, "상품 재고는 0개 이상이어야 합니다."),
+	INVALID_PRODUCT_NAME(BAD_REQUEST, "상품명은 2글자 ~ 15글자로 구성되어야 합니다."),
+	INVALID_PRODUCT_THUMBNAIL(BAD_REQUEST, "상품 썸네일 주소가 잘못되었습니다."),
+	INVALID_PRODUCT_DESCRIPTION(BAD_REQUEST, "상품 설명은 최대 100글자로 구성되어야 합니다."),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
