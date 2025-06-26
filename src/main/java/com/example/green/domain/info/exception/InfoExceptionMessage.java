42e9be09911a366f1b54de3859afa13dbeebc14b
jeehokim <rlawlgh3245@gmail.com>
2025-06-27 07:51:09 +0900

    refactor: 도메인단 및 DTO 코드 개선 작업(#40)
    * Generator Exception 과 테스트 추가

--- src/main/java/com/example/green/domain/info/exception/InfoExceptionMessage.java
+++ src/main/java/com/example/green/domain/info/exception/InfoExceptionMessage.java
@@ -12,7 +12,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum InfoExceptionMessage implements ExceptionMessage {
-	INVALID_CATEGORY_CODE(BAD_REQUEST, "유효하지 않은 카테고리 코드입니다.");
+	INVALID_CATEGORY_CODE(BAD_REQUEST, "유효하지 않은 카테고리 코드입니다."),
+	INVALID_INFO_NUMBER(BAD_REQUEST, "해당 정보 번호의 내요은 찾을 수 없습니다."),
+	CANNOT_MAKE_INFO_ID(INTERNAL_SERVER_ERROR, "정보 ID 생성 중 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
