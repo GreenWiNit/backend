79a9f5db5ab35747a38279270866a637ef85512f
jihwankim128 <jihwan1535@nate.com>
2025-06-30 08:04:34 +0900

    feat: 포인트 상품 생성 Endpoint 추가 (#31)
    * BaseControllerUnitTest 수정
    WebMvcTest Componenet Scan 범위로 인한 강한 결합 문제 식별
    각 Controller Test에서 독립적으로 WebMvcTest를 수행하도록 변경

---
+++ src/main/java/com/example/green/domain/pointshop/controller/message/PointProductResponseMessage.java
@@ -0,0 +1,14 @@
+package com.example.green.domain.pointshop.controller.message;
+
+import com.example.green.global.api.ResponseMessage;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum PointProductResponseMessage implements ResponseMessage {
+	POINT_PRODUCT_CREATION_SUCCESS("포인트 상품 생성에 성공했습니다.");
+
+	private final String message;
+}
