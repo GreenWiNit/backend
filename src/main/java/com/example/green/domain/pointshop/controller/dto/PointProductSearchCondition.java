76b19e81c562c62cedd13b08b8dfa0384a6e1c2a
jihwankim128 <jihwan1535@nate.com>
2025-07-01 17:48:02 +0900

    docs: 상품 목록 조회 swagger 문서 추가 (#60)

--- src/main/java/com/example/green/domain/pointshop/controller/dto/PointProductSearchCondition.java
+++ src/main/java/com/example/green/domain/pointshop/controller/dto/PointProductSearchCondition.java
@@ -6,13 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "상품 목록 조회 요청")
 public record PointProductSearchCondition(
-	@Schema(description = "페이지", example = "1, Can null")
+	@Schema(description = "페이지 (nullable)", example = "1")
 	Integer page,
-	@Schema(description = "페이지 당 항목 수", example = "10, Can null")
+	@Schema(description = "페이지 당 항목 수 (nullable)", example = "10", defaultValue = "10")
 	Integer size,
-	@Schema(description = "상품코드 및 상품명", example = "텀블러, Can null")
+	@Schema(description = "상품코드 및 상품명 (nullable)", example = "텀블러")
 	String keyword,
-	@Schema(description = "상품 판매 상태", example = "EXCHANGEABLE, Can null")
+	@Schema(type = "string",
+		description = "상품 판매 상태 (nullable)",
+		allowableValues = {"exchangeable", "sold_out"})
 	SellingStatus status
 ) {
 }
