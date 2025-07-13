060d5adbae3bbc7f59b60b2e297ecdc1eee76e82
jeehokim <rlawlgh3245@gmail.com>
2025-07-13 17:47:34 +0900

    refactor : 마이페이지 메인 조회 리팩토링 (#107) * 포인트에 따른 레벨 계산     * DTO 레벨 필드 추가     * 레벨 계산 메서드 추가     * Exception Message 작성 * 메인 조회 단위 테스트 * DTO/Controller docs 작성 * memberId 로 변수명 변경

--- src/main/java/com/example/green/domain/mypage/dto/MypageMainResponseDto.java
+++ src/main/java/com/example/green/domain/mypage/dto/MypageMainResponseDto.java
@@ -1,11 +1,27 @@
 package com.example.green.domain.mypage.dto;
 
-/**
- * 마이페이지 메인 응답 DTO
- * */
+import java.math.BigDecimal;
+
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "마이페이지 메인 응답 DTO")
 public record MypageMainResponseDto(
-	// TODO [확인필요] @임현정 @김지환 반환 값이 int를 주어도 되는지 확인
+
+	@DefaultValue("0")
+	@Schema(type = "int",
+		description = "회원 챌린지 참여 총 횟수")
 	int userChallengeCount,
-	int userTotalPoints
+
+	@DefaultValue("0")
+	@Schema(type = "BigDecimal",
+		description = "회원 총 보유 포인트")
+	BigDecimal userTotalPoints,
+
+	@DefaultValue("1")
+	@Schema(type = "int",
+		description = "회원 레벨(현 포인트로 계산된 값)")
+	int userLevel
 ) {
 }
