95cb50f888f78dd7a2c1bc5c379e5c0bad3af9f2
jihwankim128 <jihwan1535@nate.com>
2025-08-12 19:00:37 +0900

    docs: 스웨거  추가

--- src/main/java/com/example/green/domain/certification/ui/ChallengeCertificationController.java
+++ src/main/java/com/example/green/domain/certification/ui/ChallengeCertificationController.java
@@ -10,24 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.green.domain.certification.application.ChallengeCertificationService;
 import com.example.green.domain.certification.application.command.PersonalChallengeCertificateCommand;
 import com.example.green.domain.certification.application.command.TeamChallengeCertificateCommand;
+import com.example.green.domain.certification.ui.docs.ChallengeCertificationControllerDocs;
 import com.example.green.domain.certification.ui.dto.PersonalChallengeCertificateDto;
 import com.example.green.domain.certification.ui.dto.TeamChallengeCertificateDto;
 import com.example.green.global.api.NoContent;
 import com.example.green.global.security.PrincipalDetails;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/certifications/challenges")
-public class ChallengeCertificationController {
+public class ChallengeCertificationController implements ChallengeCertificationControllerDocs {
 
 	private final ChallengeCertificationService challengeCertificationService;
 
 	@PostMapping("/personal/{challengeId}")
 	public NoContent certificateTeamChallenge(
 		@PathVariable Long challengeId,
-		@RequestBody PersonalChallengeCertificateDto dto,
+		@Valid @RequestBody PersonalChallengeCertificateDto dto,
 		@AuthenticationPrincipal PrincipalDetails principalDetails
 	) {
 		Long memberId = principalDetails.getMemberId();
@@ -39,7 +41,7 @@ public class ChallengeCertificationController {
 	@PostMapping("/team/{groupId}")
 	public NoContent certificateTeamChallenge(
 		@PathVariable Long groupId,
-		@RequestBody TeamChallengeCertificateDto dto,
+		@Valid @RequestBody TeamChallengeCertificateDto dto,
 		@AuthenticationPrincipal PrincipalDetails principalDetails
 	) {
 		Long memberId = principalDetails.getMemberId();
---
+++ src/main/java/com/example/green/domain/certification/ui/docs/ChallengeCertificationControllerDocs.java
@@ -0,0 +1,36 @@
+package com.example.green.domain.certification.ui.docs;
+
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.*;
+
+import com.example.green.domain.certification.ui.dto.PersonalChallengeCertificateDto;
+import com.example.green.domain.certification.ui.dto.TeamChallengeCertificateDto;
+import com.example.green.global.api.NoContent;
+import com.example.green.global.docs.ApiErrorStandard;
+import com.example.green.global.security.PrincipalDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "[챌린지 인증-클라이언트] 챌린지 인증 API", description = "챌린지 인증 관련 API")
+public interface ChallengeCertificationControllerDocs {
+
+	@Operation(summary = "개인 챌린지 인증 (B01_004)", description = "챌린지 인증")
+	@ApiErrorStandard
+	@ApiResponse(responseCode = "200", description = "개인 챌린지 인증 성공")
+	NoContent certificateTeamChallenge(
+		@Parameter(description = "챌린지 ID", in = PATH, required = true, example = "1") Long challengeId,
+		PersonalChallengeCertificateDto dto,
+		PrincipalDetails principalDetails
+	);
+
+	@Operation(summary = "팀 챌린지 인증 (B01_006)", description = "팀 챌린지 인증")
+	@ApiErrorStandard
+	@ApiResponse(responseCode = "200", description = "팀 챌린지 인증 성공")
+	NoContent certificateTeamChallenge(
+		@Parameter(description = "그룹 ID", in = PATH, required = true, example = "1") Long groupId,
+		TeamChallengeCertificateDto dto,
+		PrincipalDetails principalDetails
+	);
+}
