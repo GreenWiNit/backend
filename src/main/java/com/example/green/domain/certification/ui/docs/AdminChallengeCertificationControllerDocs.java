b75582c26b332030fdf7e4f4f3fa2c403c337ec8
jihwankim128 <jihwan1535@nate.com>
2025-08-13 09:33:22 +0900

    docs: 챌린지 인증 스웨거 문서화 추가

--- src/main/java/com/example/green/domain/certification/ui/docs/AdminChallengeCertificationControllerDocs.java
+++ src/main/java/com/example/green/domain/certification/ui/docs/AdminChallengeCertificationControllerDocs.java
@@ -2,7 +2,9 @@ package com.example.green.domain.certification.ui.docs;
 
 import com.example.green.domain.certification.infra.filter.ChallengeCertificationFilter;
 import com.example.green.domain.certification.ui.dto.AdminCertificateSearchDto;
+import com.example.green.domain.certification.ui.dto.CertificationIdentifierDto;
 import com.example.green.global.api.ApiTemplate;
+import com.example.green.global.api.NoContent;
 import com.example.green.global.api.page.PageTemplate;
 import com.example.green.global.docs.ApiErrorStandard;
 
@@ -17,4 +19,14 @@ public interface AdminChallengeCertificationControllerDocs {
 	@ApiErrorStandard
 	@ApiResponse(responseCode = "200", description = "챌린지 인증 목록 조회에 성공헀습니다.")
 	ApiTemplate<PageTemplate<AdminCertificateSearchDto>> getCertifications(ChallengeCertificationFilter filter);
+
+	@Operation(summary = "챌린지 인증 목록 포인트 지급 (ad_B01_003, ad_B01_006)", description = "챌린지 포인트 지급")
+	@ApiErrorStandard
+	@ApiResponse(responseCode = "200", description = "챌린지 인증 포인트 지급 처리에 성공헀습니다.")
+	NoContent approveCertification(CertificationIdentifierDto dto);
+
+	@Operation(summary = "챌린지 인증 목록 포인트 미지급 (ad_B01_003, ad_B01_006)", description = "챌린지 포인트 미지급")
+	@ApiErrorStandard
+	@ApiResponse(responseCode = "200", description = "챌린지 인증 포인트 미지급에 처리 성공헀습니다.")
+	NoContent rejectCertification(CertificationIdentifierDto dto);
 }
