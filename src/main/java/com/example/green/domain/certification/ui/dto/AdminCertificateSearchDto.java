4fcebc59387c9b2cf852ae33b65bcb5730e5f087
jihwankim128 <jihwan1535@nate.com>
2025-08-19 01:26:13 +0900

    refactor: challenge id 제거 및 dto 필드 resource 중복 제거

--- src/main/java/com/example/green/domain/certification/ui/dto/AdminCertificateSearchDto.java
+++ src/main/java/com/example/green/domain/certification/ui/dto/AdminCertificateSearchDto.java
@@ -6,6 +6,7 @@ import com.example.green.domain.certification.domain.CertificationStatus;
 import com.example.green.domain.certification.domain.ChallengeSnapshot;
 import com.example.green.domain.certification.domain.MemberSnapshot;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,22 +14,58 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AdminCertificateSearchDto {
 	private Long id;
-	private Long challengeId;
-	private ChallengeSnapshot challenge;
-	private MemberSnapshot member;
+	private ChallengeInfo challenge;
+	private MemberInfo member;
 	private LocalDate certifiedDate;
 	private String imageUrl;
 	private String review;
 	private CertificationStatus status;
+
+	@Data
+	@AllArgsConstructor
+	public static class ChallengeInfo {
+		private Long id;
+		private String name;
+		private String code;
+		private String image;
+		private Integer point;
+		private String groupCode;
+		private String type;
+
+		public static ChallengeInfo from(ChallengeSnapshot snapshot) {
+			return new ChallengeInfo(
+				snapshot.getChallengeId(),
+				snapshot.getChallengeName(),
+				snapshot.getChallengeCode(),
+				snapshot.getChallengeImage(),
+				snapshot.getChallengePoint(),
+				snapshot.getGroupCode(),
+				snapshot.getType()
+			);
+		}
+	}
+
+	@Data
+	@AllArgsConstructor
+	public static class MemberInfo {
+		private Long id;
+		private String key;
+
+		public static MemberInfo from(MemberSnapshot snapshot) {
+			return new MemberInfo(
+				snapshot.getMemberId(),
+				snapshot.getMemberKey()
+			);
+		}
+	}
 
 	public AdminCertificateSearchDto(
 		Long id, ChallengeSnapshot challenge, MemberSnapshot member, LocalDate certifiedDate,
 		String imageUrl, String review, CertificationStatus status
 	) {
 		this.id = id;
-		this.challengeId = challenge.getChallengeId();
-		this.challenge = challenge;
-		this.member = member;
+		this.challenge = ChallengeInfo.from(challenge);
+		this.member = MemberInfo.from(member);
 		this.certifiedDate = certifiedDate;
 		this.imageUrl = imageUrl;
 		this.review = review;
