76d2c34fb9c29706182f0725d4136ba03d67a96d
jihwankim128 <jihwan1535@nate.com>
2025-10-26 18:19:05 +0900

    refactor: 사용하지 않는 메서드 제거 ISP 준수

--- src/main/java/com/example/green/domain/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/example/green/domain/challenge/repository/ChallengeRepository.java
@@ -3,10 +3,8 @@ package com.example.green.domain.challenge.repository;
 import static com.example.green.domain.challenge.exception.ChallengeExceptionMessage.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.green.domain.challenge.entity.challenge.Challenge;
-import com.example.green.domain.challenge.entity.challenge.vo.ChallengeType;
 import com.example.green.domain.challenge.exception.ChallengeException;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
@@ -14,16 +12,4 @@ public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 	default Challenge findByIdWithThrow(Long id) {
 		return findById(id).orElseThrow(() -> new ChallengeException(CHALLENGE_NOT_FOUND));
 	}
-
-	@Query("SELECT COUNT(p) FROM Participation p "
-		+ "WHERE p.challenge.id = :personalChallengeId "
-		+ "AND p.challenge.type = :type")
-	long countParticipantByChallenge(Long challengeId, ChallengeType type);
-
-	@Query("""
-		SELECT count(p) > 0 FROM Participation p
-		WHERE p.challenge.id = :challengeId
-		AND p.memberId =:memberId
-		""")
-	boolean existsMembership(Long challengeId, Long memberId);
 }
