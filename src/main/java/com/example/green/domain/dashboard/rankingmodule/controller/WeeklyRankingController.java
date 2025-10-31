d77cd26d125fae4d079c9bf77b54b1fa2657d29a
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-31 18:39:42 +0900

    fix: Swagger 문서 수정

--- src/main/java/com/example/green/domain/dashboard/rankingmodule/controller/WeeklyRankingController.java
+++ src/main/java/com/example/green/domain/dashboard/rankingmodule/controller/WeeklyRankingController.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +19,12 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/dashboard/weekly-ranking")
 public class WeeklyRankingController implements WeeklyRankingControllerDocs {
 
 	private final WeeklyRankingService weeklyRankingService;
 
-	@GetMapping("/api/dashboard/weekly-ranking")
-	@Override
+	@GetMapping
 	public ApiTemplate<LoadWeeklyRankingResponse> getWeeklyRanking(
 		@RequestParam("weekStart")
 		@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate weekStart,
@@ -32,9 +33,9 @@ public class WeeklyRankingController implements WeeklyRankingControllerDocs {
 		Long currentMemberId = principal.getMemberId();
 		int topN = 8;
 
-		LoadWeeklyRankingResponse response = weeklyRankingService.loadWeeklyRanking(weekStart, topN, currentMemberId);
+		LoadWeeklyRankingResponse response =
+			weeklyRankingService.loadWeeklyRanking(weekStart, topN, currentMemberId);
 
 		return ApiTemplate.ok(WeeklyRankingResponseMessage.LOAD_WEEKLY_RANKING_SUCCESS, response);
 	}
-	
 }
