13bca699d9a7ed2430b566a95c394a96cf0e32fe
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-11-18 23:30:04 +0900

    fix: 성장 데이터 조회시 404 에러해결

--- src/main/java/com/example/green/domain/dashboard/growth/service/GrowthService.java
+++ src/main/java/com/example/green/domain/dashboard/growth/service/GrowthService.java
@@ -11,6 +11,7 @@ import com.example.green.domain.dashboard.growth.entity.enums.Level;
 import com.example.green.domain.dashboard.growth.exception.GrowthException;
 import com.example.green.domain.dashboard.growth.message.GrowthExceptionMessage;
 import com.example.green.domain.dashboard.growth.repository.GrowthRepository;
+import com.example.green.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,13 +22,25 @@ public class GrowthService {
 
 	private final GrowthRepository growthRepository;
 	private final GrowthCalculateService calculateService;
+	private final MemberRepository memberRepository;
 
 	public LoadGrowthResponse loadGrowth(Long memberId) {
 
 		calculateService.calculateMemberGrowth(memberId);
 
-		Growth growth = growthRepository.findByMemberId(memberId)
+		memberRepository.findById(memberId)
 			.orElseThrow(() -> new GrowthException(GrowthExceptionMessage.NOT_FOUND_USER));
+
+		Growth growth = growthRepository.findByMemberId(memberId)
+			.orElseGet(() -> {
+				Growth newGrowth = Growth.builder()
+					.level(Level.SOIL)
+					.progress(BigDecimal.ZERO)
+					.requiredPoint(BigDecimal.valueOf(250))
+					.goalLevel(Level.SPROUT)
+					.build();
+				return growthRepository.save(newGrowth);
+			});
 
 		Level nextLevel;
 		BigDecimal nextLevelPoint;
