50cb4f271a63a77cd21be744422add2dc3a7145f
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-11-23 00:42:04 +0900

    fix: PointItemOrderServiceControllerTest 테스트 코드 수정

--- src/main/java/com/example/green/domain/pointshop/item/dto/request/OrderPointItemRequest.java
+++ src/main/java/com/example/green/domain/pointshop/item/dto/request/OrderPointItemRequest.java
@@ -1,12 +1,14 @@
 package com.example.green.domain.pointshop.item.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "아이템 상품 주문 요청")
 public record OrderPointItemRequest(
 
-	@NotBlank(message = "아이템 상품 주문수는 비어 있을 수 없습니다.")
+	@NotNull(message = "아이템 상품 주문수는 비어 있을 수 없습니다.")
+	@Min(value = 1, message = "아이템 상품 주문수는 1개 이상이어야 합니다.")
 	@Schema(description = "아이템 상품 수", example = "3")
 	Integer amount
 ) {
