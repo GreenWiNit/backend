d45ac4a6d4894dd1be8adb8638890c37785e8866
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-04 17:08:45 +0900

    fix: Application Schedular 활성화

--- src/main/java/com/example/green/domain/dashboard/rankingmodule/service/WeeklyRankingScheduler.java
+++ src/main/java/com/example/green/domain/dashboard/rankingmodule/service/WeeklyRankingScheduler.java
@@ -8,9 +8,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WeeklyRankingScheduler {
 
 	private final WeeklyRankingService weeklyRankingService;
@@ -18,8 +20,11 @@ public class WeeklyRankingScheduler {
 	/**
 	 * 매주 월요일 00:00시 지난 주 랭킹 계산
 	 */
-	@Scheduled(cron = "0 0 17 * * THU", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 10 17 * * THU", zone = "Asia/Seoul")
 	public void calculateWeeklyRankingBatch() {
+
+		log.info("스케줄러 작동");
+
 		LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
 
 		// 오늘이 월요일 → 지난주 계산
