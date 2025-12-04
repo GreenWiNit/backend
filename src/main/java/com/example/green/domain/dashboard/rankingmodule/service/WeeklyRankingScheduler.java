5fe229e2192c410741065c2488b9397d8b3c8c51
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-04 17:12:57 +0900

    fix: Schedular 시간 수정

--- src/main/java/com/example/green/domain/dashboard/rankingmodule/service/WeeklyRankingScheduler.java
+++ src/main/java/com/example/green/domain/dashboard/rankingmodule/service/WeeklyRankingScheduler.java
@@ -20,7 +20,7 @@ public class WeeklyRankingScheduler {
 	/**
 	 * 매주 월요일 00:00시 지난 주 랭킹 계산
 	 */
-	@Scheduled(cron = "0 10 17 * * THU", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 15 17 * * THU", zone = "Asia/Seoul")
 	public void calculateWeeklyRankingBatch() {
 
 		log.info("스케줄러 작동");
